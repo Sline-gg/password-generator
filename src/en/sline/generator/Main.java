82281a30b1a56747128bfab78447c6418e085911
Sline-gg <dimka.kosuhin@gmail.com>
2020-10-11 00:34:18 +0300

    Added to this programm gui, but not visual this gui interface

--- src/en/sline/generator/Main.java
+++ src/en/sline/generator/Main.java
@@ -1,13 +1,30 @@
 package en.sline.generator;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 
-public class Main {
+public class Main extends JFrame{
+    JPanel panel;
+    JLabel label;
 
     public static void main(String[] args) {
         int length = 32;
+        JFrame jf = new JFrame();
         System.out.println(generatePassword(length));
     }
+
+    // constructor
+    Main(String title ) {
+        super( title );                      // invoke the JFrame constructor
+        setSize( 150, 100 );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        setLayout( new FlowLayout() );       // set the layout manager
+        label = new JLabel("Hello Swing!");  // construct a JLabel
+        add( label );                        // add the label to the JFrame
+    }
+
 
     public static char[] generatePassword(int len){
         System.out.println("Your Password:");
