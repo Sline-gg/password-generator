a11f58fe40c5621f5c491c030ae962ac525a0900
Sline-gg <dimka.kosuhin@gmail.com>
2020-11-18 14:48:38 +0300

    Change interface programm, but not create password

--- src/en/sline/generator/gui/SimpleGui.java
+++ src/en/sline/generator/gui/SimpleGui.java
@@ -7,9 +7,11 @@ import java.awt.event.ActionListener;
 
 public class SimpleGui extends JFrame {
 
-    private JButton button = new JButton("Generate");
+    private JButton button = new JButton("Press");
     private JTextField input = new JTextField("", 5);
-    private JLabel label = new JLabel("Password:");
+    private JLabel label = new JLabel("Input:");
+    private JRadioButton radio1 = new JRadioButton("Select this");
+    private JRadioButton radio2 = new JRadioButton("Select that");
     private JCheckBox check = new JCheckBox("Check", false);
 
     public SimpleGui() {
